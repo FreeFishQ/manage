9ff084621d275ddf1dbd2a4ff57ca303d555248b
FreeFishQ <hejd0811@163.com>
2023-05-17 23:14:28 +0800

    整体布局

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,98 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="height: 100%">
+    <el-container style="height:100%; ">
+    <el-aside :width="sidewidth+'px'" style="background-color: rgb(238, 241, 246);overflow-x: hidden" >
+      <el-menu :default-openeds="['1', '3']" style="height: 100% ; overflow-x: hidden;"
+               background-color="rgb(48,65,86)" text-color="#fff"
+                active-text-color="#ffd04b"
+               :collapse-transition="false"
+               :collapse="isCollapse"
+      >
+        <el-submenu index="1">
+          <template slot="title"><i class="el-icon-message"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title"><i class="el-icon-menu"></i>
+            <span>导航二</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title"><i class="el-icon-setting"></i>
+            <span>导航三</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="3-1">选项1</el-menu-item>
+            <el-menu-item index="3-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="3-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="3-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-header style=" font-size: 12px; border-bottom: 1px solid #ccc;line-height:60px;display: flex " >
+        <div style="flex: 1;font-size: 18px">
+          <span :class="collapseBtnClass" style="cursor: pointer" @click="collapse"></span>
+        </div>
+        <el-dropdown style="width: 70px; cursor: pointer">
+          <span>王小虎</span>
+          <i class="el-icon-arrow-down" style="margin-left: 5px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人信息</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+
+          </el-dropdown-menu>
+        </el-dropdown>
+
+      </el-header>
+
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column prop="date" label="日期" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="地址">
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </el-container>
+
+
   </div>
 </template>
 
@@ -13,6 +104,27 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data() {
+    const item = {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    };
+    return {
+      tableData: Array(10).fill(item),
+      collapseBtnClass:'el-icon-s-fold',
+      isCollapse:false,
+      sidewidth:200
+    }
+  },
+  methods:{
+    collapse:function (){//点击收缩按钮触发
+      this.isCollapse=!this.isCollapse;
+      if(this.isCollapse){
+        this.sidewidth=64
+      }
+    }
   }
 }
 </script>
