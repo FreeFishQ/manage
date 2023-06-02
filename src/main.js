be4fe2cfb19a24122675d888469e028994a530d4
FreeFishQ <hejd0811@163.com>
2023-06-02 17:23:17 +0800

    vue实现增删改查

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/global.css'
+import {request} from "axios";
+import  requset from "@/utils/request";
+import Request from "@/utils/request";
 
 
 Vue.config.productionTip = false
 Vue.use(ElementUI,{size:"small"});
+
+Vue.prototype.request=Request
 new Vue({
   router,
   render: h => h(App)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -76,17 +76,31 @@
         </div>
         <div style="padding: 10px 0">
           <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search" v-model="username"></el-input>
-<!--          <el-input style="width: 200px" placeholder="请输入邮箱" suffix-icon="el-icon-message" class="ml-5"></el-input>-->
-<!--          <el-input style="width: 200px" placeholder="请输入地址" suffix-icon="el-icon-position" class="ml-5"></el-input>-->
+          <el-input style="width: 200px" placeholder="请输入邮箱" suffix-icon="el-icon-message" class="ml-5" v-model="email"></el-input>
+          <el-input style="width: 200px" placeholder="请输入地址" suffix-icon="el-icon-position" class="ml-5" v-model="address"></el-input>
           <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
+          <el-button class="ml-5" type="warning" @click="reset">重置</el-button>
         </div>
         <div style="margin: 10px 0">
-          <el-button type="primary">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
-          <el-button type="danger">批量删除 <i class="el-icon-remove-outline"></i></el-button>
-          <el-button type="primary">导入<i class="el-icon-bottom"></i></el-button>
+          <el-button type="primary" @click="handleAdd">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
+          <el-popconfirm
+              class="ml-5"
+              confirm-button-text='确定'
+              cancel-button-text='我再想想'
+              icon="el-icon-info"
+              icon-color="red"
+              title="您确定批量删除吗？"
+              @confirm="delBatch"
+
+          >
+          <el-button type="danger" slot="reference">批量删除 <i class="el-icon-remove-outline"></i></el-button>
+          </el-popconfirm>
+          <el-button type="primary" class="ml-5">导入<i class="el-icon-bottom"></i></el-button>
           <el-button type="primary">导出 <i class="el-icon-top"></i></el-button>
         </div>
-        <el-table :data="tableData" border stripe :header-cell-class-name="headerBg">
+        <el-table :data="tableData" border stripe :header-cell-class-name="headerBg"  @selection-change="handleSelectionChange">>
+          <el-table-column type="selection" width="55">
+          </el-table-column>
           <el-table-column prop="id" label="ID" width="80">
           </el-table-column>
           <el-table-column prop="username" label="用户名" width="140">
@@ -101,23 +115,60 @@
           </el-table-column>
           <el-table-column label="操作">
             <template slot-scope="scope">
-              <el-button type="success">编辑 <i class="el-icon-edit"></i></el-button>
-              <el-button type="danger">删除 <i class="el-icon-remove-outline"></i></el-button>
+              <el-button type="success" @click="handleEdit(scope.row)">编辑 <i class="el-icon-edit"></i></el-button>
+              <el-popconfirm
+                  class="ml-5"
+                  confirm-button-text='确定'
+                  cancel-button-text='我再想想'
+                  icon="el-icon-info"
+                  icon-color="red"
+                  title="您确定删除吗？"
+                  @confirm="handleDelete(scope.row.id)"
+              >
+              <el-button type="danger" slot="reference">删除 <i class="el-icon-remove-outline"></i></el-button>
+              </el-popconfirm>
             </template>
           </el-table-column>
         </el-table>
+        <div style="padding: 10px 30px">
+          <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="pageNum"
+              :page-sizes="[2, 5, 10, 20]"
+              :page-size="pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="total">
+          </el-pagination>
+        </div>
+
+        <el-dialog title="用户信息" :visible.sync="dialogFormVisible" width="30%">
+          <el-form label-width="70px" size="small">
+            <el-form-item label="用户名" >
+              <el-input v-model="form.username" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="昵称" >
+              <el-input v-model="form.nickname" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" >
+              <el-input v-model="form.email" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="电话" >
+              <el-input v-model="form.phone" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="地址" >
+              <el-input v-model="form.address" autocomplete="off"></el-input>
+            </el-form-item>
+
+
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="save">确 定</el-button>
+          </div>
+        </el-dialog>
       </el-main>
-      <div style="padding: 10px 30px">
-        <el-pagination
-            @size-change="handleSizeChange"
-            @current-change="handleCurrentChange"
-            :current-page="pageNum"
-            :page-sizes="[2, 5, 10, 20]"
-            :page-size="pageSize"
-            layout="total, sizes, prev, pager, next, jumper"
-            :total="total">
-        </el-pagination>
-      </div>
+
     </el-container>
   </el-container>
 
@@ -128,6 +179,8 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+// import {request} from "axios";
+import request from "@/utils/request";
 
 export default {
   name: 'HomeView',
@@ -142,6 +195,11 @@ export default {
       pageNum:1,
       pageSize:2,
       username:"",
+      email:"",
+      address:"",
+      form:{},
+      dialogFormVisible:false,
+      multipleSelection:[],
       collapseBtnClass:'el-icon-s-fold',
       isCollapse:false,
       sidewidth:200,
@@ -166,12 +224,81 @@ export default {
       }
     },
     load(){
-      //   请求分页查询数据
-      fetch("http://localhost:8087/user/page?pageNum="+this.pageNum+"&pageSize="+this.pageSize+"&username="+this.username).then(res=>res.json()).then(res=>{
-        console.log(res)
-        this.tableData=res.data
+          // ?pageNum="+this.pageNum+"&pageSize="+this.pageSize+"&username="+this.username
+      request.get("/user/page",{
+        params:{
+          pageNum:this.pageNum,
+          pageSize:this.pageSize,
+          username:this.username,
+          email:this.email,
+          address:this.address,
+        }
+          }
+      ).then(res=> {
+      console.log(res)
+        this.tableData=res.records
         this.total=res.total
+            //   请求分页查询数据
+      // fetch("http://localhost:8087/user/page?pageNum="+this.pageNum+"&pageSize="+this.pageSize+"&username="+this.username).then(res=>res.json()).then(res=>{
+      //   console.log(res)
+      //   this.tableData=res.data
+      //   this.total=res.total
       })
+    },
+    reset(){
+      this.username=""
+      this.email=""
+      this.address=""
+      this.load()
+    },
+    handleAdd(){
+      this.dialogFormVisible=true
+      this.form={}
+    },
+    handleEdit(row){
+      this.form=Object.assign({},row)
+      this.dialogFormVisible=true
+
+    },
+    handleDelete(id){
+      request.delete("/user/"+id).then (res=>{
+        if(res){
+          this.$message.success("删除成功")
+
+          this.load()
+        }else{
+          this.$message.error("删除失败")
+        }
+      })
+    },
+    delBatch(){
+      let ids=this.multipleSelection.map(v=>v.id)
+      request.post("/user/del/batch",ids).then (res=>{
+        if(res){
+          this.$message.success("批量删除成功")
+
+          this.load()
+        }else{
+          this.$message.error("批量删除失败")
+        }
+      })
+    },
+    save(){
+      request.post("/user",this.form).then(res=>{
+        if(res){
+          this.$message.success("保存成功")
+          this.dialogFormVisible=false
+          this.load()
+        }else{
+          this.$message.error("保存失败")
+        }
+
+      })
+    },
+    handleSelectionChange(val){
+      // console.log(val)
+      this.multipleSelection=val
+
     },
     handleSizeChange(pageSize){
       this.pageSize=pageSize
